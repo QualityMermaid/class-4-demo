570515a877830355ec44d140e231bcbb1eb049d2
JessicaBane <jessicambane@gmail.com>
2023-07-20 11:23:40 +0100

    updated files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
+  const [formData, setFormData] = useState({
+    name:"",
+    dob:""
+  });
+
+  function handleFormData(event){
+    // console.log(event.target.value)
+    // console.log(event.target.name)
+    // console.log("form changed")
+    // [event.target.name] is used as a prop as the event.target.value is a string
+    setFormData({...formData, [event.target.name]:event.target.value})
+    // console.log(typeof(event.target.value))
+    console.log(formData)
+  }
+
+  function handleFormSubmit(event){
+    event.preventDefault()
+    console.log(formData)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+
+    <form onSubmit={handleFormSubmit}>
+      <legend>  </legend>
+      <label htmlFor='name'>Name:</label>
+      <input type='text' id='name' name='name' placeholder='enter your name' onChange={handleFormData}></input>
+      <label htmlFor='dob'> Date of birth:</label>
+      <input type='date' id='dob' name='dob' onChange={handleFormData}></input>
+      <button type='submit'>Submit</button>
+    </form>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Header.css"
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Forms in React</h1>
+      </header>
+  )
+}
